cffdf718eb34feb935f1ff429a49ca7867cb22e1
santirondini <srondini@frba.utn.edu.ar>
2025-04-10 01:39:59 -0300

    por ahora

--- client/main.go
+++ client/main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"client/globals"
 	"client/utils"
+	"log"
 )
 
 func main() {
+	
+	
 	utils.ConfigurarLogger()
 
 	// loggear "Hola soy un log" usando la biblioteca log
+	log.Println("Hola soy un log")
 	globals.ClientConfig = utils.IniciarConfiguracion("config.json")
 	// validar que la config este cargada correctamente
-
+	
 	// loggeamos el valor de la config
 
 	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
--- client/utils/utils.go
+++ client/utils/utils.go
@@ -83,9 +83,13 @@ func EnviarPaquete(ip string, puerto int, paquete Paquete) {
 
 func ConfigurarLogger() {
 	logFile, err := os.OpenFile("tp0.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
-	if err != nil {
-		panic(err)
+	if err != nil { // si err no es nil, significa que ocurrió un error al intentar abrir/crear el archivo
+		panic(err) //detiene la ejecución del programa y muestra el error. El programa no continua si no se puede configuarar el logger
 	}
-	mw := io.MultiWriter(os.Stdout, logFile)
-	log.SetOutput(mw)
+	mw := io.MultiWriter(os.Stdout, logFile) // se crea un escritor Writer que envia los datos a multiples destinos. 
+	log.SetOutput(mw) // se configura para que le logger envie mensajes a mw, que a suvez los redirige tanto a consola como a tp0.log
 }
+
+// os.Stdout representa la salida estandar (consola)
+// mv es un escritor combinado que enviará cualquier mensaje de log tanto a consola como al archivo
+// Ejemplo de uso: log.Println("Este es un mensaje de prueba")
